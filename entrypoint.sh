9e35d43e491c8cd5b9f3307c5acd57a317e08fd1
Julian Chen <julianchen606@gmail.com>
2025-03-05 11:05:30 -0800

    temp change

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-python scripts/db_setup.py
-python scripts/import_data.py
+# python scripts/db_setup.py
+# python scripts/import_data.py
 
 CMD_STR="uvicorn app.main:app --host 0.0.0.0 --port 8000"
 exec /bin/bash -c "$CMD_STR"
